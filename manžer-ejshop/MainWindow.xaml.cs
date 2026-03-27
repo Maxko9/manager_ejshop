337449dcf81b1ecf56cb9b688db753d94ef95048
Maxko9 <maxik.kunc09@gmail.com>
2026-03-27 13:53:40 +0100

    Přidejte soubory projektu.

---
+++ manžer-ejshop/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace manžer_ejshop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Produkt> Sklad = new ObservableCollection<Produkt> ();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Sklad.Add(new Produkt(20, "Brumík", 50));
+            Sklad.Add(new Produkt(30, "Kofola", 100));
+            Sklad.Add(new Produkt(20, "Vinea", 37));
+            Sklad.Add(new Produkt(3, "Rohlajz", 500));
+            Sklad.Add(new Produkt(20, "Šunka", 100));
+            Sklad.Add(new Produkt(40, "Máslo", 25));
+
+            dgSklad.ItemsSource = Sklad;
+        }
+
+        //tlacitko pridat do skladu
+        private void btnPridatDoSkladu_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNovyProductName.Text) && double.TryParse(txtNovyProductCena.Text, out double cena))
+            {
+                Sklad.Add(new Produkt(cena, txtNovyProductName.Text, 10));
+                txtNovyProductCena.Clear();
+                txtNovyProductName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Zadejte správné jméno a cenu!!!!!");
+            }
+        }
+    }
+}
---
+++ manžer-ejshop/Produkt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manžer_ejshop
+{
+    internal class Produkt
+    {
+        public string nazev { get; set; }
+        public double cena {  get; set; }
+        public int mnozstvi { get; set; }
+
+        public Produkt(double cena, string nazev, int mnozstvi)
+        {
+            this.cena = cena;
+            this.nazev = nazev;
+            this.mnozstvi = mnozstvi;
+        }
+
+    }
+}
